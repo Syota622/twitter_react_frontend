3e8d17bf89db0e51bc5a966cd1c80f797addbad4
Shota <wvp360gqanukcqhg@gmail.com>
2024-04-20 10:28:43 +0900

    実装完了

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import TweetForm from "../organisms/TweetForm";
+import axios from "../../utils/axios"; // カスタムインスタンスをインポート
 
 const SidebarContainer = styled.div`
   flex: 1;
@@ -44,6 +45,33 @@ const useUserId = () => {
 
 const Sidebar = () => {
   const userId = useUserId(); // ログインユーザーのIDを取得
+  const navigate = useNavigate();
+  const [isDeleted, setIsDeleted] = useState(false); // ユーザーが退会したかどうかを表すstate
+
+  // 退会APIを呼び出す関数
+  const deleteUser = async () => {
+    try {
+      const response = await axios.delete(`/users/delete`);
+      if (response.status === 200) {
+        alert("退会に成功しました");
+        // ユーザーをログアウト
+        localStorage.removeItem("id");
+        localStorage.removeItem("token");
+        // ユーザーが退会したことを表すstateを更新
+        setIsDeleted(true);
+      }
+    } catch (error) {
+      console.error("退会に失敗しました:", error);
+    }
+  };
+
+  // ユーザーが退会したときに画面遷移を行う
+  useEffect(() => {
+    if (isDeleted) {
+      navigate("/");
+      window.location.reload();
+    }
+  }, [isDeleted, navigate]);
 
   return (
     <SidebarContainer>
@@ -53,6 +81,7 @@ const Sidebar = () => {
       <StyledLink>設定</StyledLink>
       <StyledLink>ログアウト</StyledLink>
       {userId && <StyledLink to={`/user/${userId}`}>プロフィール</StyledLink>}
+      <StyledLink onClick={() => deleteUser(userId)}>退会</StyledLink>
       <TweetForm />
     </SidebarContainer>
   );
